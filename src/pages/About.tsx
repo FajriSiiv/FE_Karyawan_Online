85ff60c7e4df04473402c8184d268b2d6b50c6e6
Fajri Siiv <fajrijijin@gmail.com>
2023-01-21 19:10:03 +0700

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="container">
-      <h1>About</h1>
+    <div className="container mx-auto flex flex-col gap-y-4">
+      <h1 className="text-3xl font-semibold text-center">About</h1>
       <p>1.Dibuat dengan stack : MongoDB,Node.JS,Express, dan ReactJS</p>
       <p>
         2.Menggunakan State Management Seperti Zustand untuk mengelola API Data.
